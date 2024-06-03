897216a2b3385ef10962e16f72ddba9b1d5d8249
Aren <a00123561574@gmail.com>
2024-06-03 22:16:04 +0800

    创建及配置新输入系统

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerInputControl inputControl;
+    public Vector2 inputDirection;
+
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+    public void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
 }
