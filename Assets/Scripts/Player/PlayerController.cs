eec324eb598441bb2874b0474435fbaf3181d912
Aren <a00123561574@gmail.com>
2024-06-04 22:36:06 +0800

    实现人物移动和翻转

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
     public Vector2 inputDirection;
+    public float speed;
 
     private void Awake()
     {
         inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
@@ -25,5 +28,29 @@ public class PlayerController : MonoBehaviour
     public void Update()
     {
         inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;
+
+        if (inputDirection.x > 0)
+        {
+            faceDir = 1;
+        }
+        if (inputDirection.x < 0)
+        {
+            faceDir = -1;
+        }
+        //人物翻轉
+
+        transform.localScale = new Vector3(faceDir, 1, 1);
     }
 }
