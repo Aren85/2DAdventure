90674b360d10b2717c956e4ba3f52ef9fdfd6da3
Aren <a00123561574@gmail.com>
2024-06-05 23:03:53 +0800

    物理环境监测及Gizmos绘制

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
     public Vector2 inputDirection;
     [Header("基本參數")]
     public float speed;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         inputControl = new PlayerInputControl();
 
         inputControl.Gameplay.Jump.started += Jump;
@@ -63,6 +65,9 @@ public class PlayerController : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        if (physicsCheck.isGround)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
