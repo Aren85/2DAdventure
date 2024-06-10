786e029101c3d08b58e2fb30a238088b5b65cfa3
Aren <a00123561574@gmail.com>
2024-06-10 22:38:14 +0800

    野猪 - 撞墙判定和等候计时

--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
     [Header("檢測參數")]
+    public bool manual;
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public float checkRaduis;
     public LayerMask groundLayer;
     [Header("狀態")]
     public bool isGround;
+    public bool touchLeftWall;//碰撞到左牆
+    public bool touchRightWall;//碰撞到右牆
+
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (!manual)
+        {
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+        }
+    }
     void Update()
     {
         Check();
@@ -19,10 +35,16 @@ public class PhysicsCheck : MonoBehaviour
     {
         //檢測地面
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+
+        //牆體判斷
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
     }
 }
