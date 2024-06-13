d38f46e2ce2e52a47173d23dce188f3cdc09e050
Aren <a00123561574@gmail.com>
2024-06-14 00:17:55 +0800

    UI - 血量更新逻辑的实现(1)

---
+++ Assets/Scripts/UI/PlayerStatBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatBar : MonoBehaviour
+{
+    public Image healthImage;
+    public Image healthDelayImage;
+    public Image powerImage;
+
+    /// <summary>
+    /// 接受Health的變更百分比
+    /// </summary>
+    /// <param name="persentage">百分比:Current/max</param>
+    public void OnHealthChange(float persentage)
+    {
+        healthImage.fillAmount = persentage;
+    }
+}
