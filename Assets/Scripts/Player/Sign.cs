22b022605ada81147a35801087a561da479d40c0
Aren <a00123561574@gmail.com>
2024-06-15 22:29:21 +0800

    人物可互动标识

---
+++ Assets/Scripts/Player/Sign.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+
+public class Sign : MonoBehaviour
+{
+    private PlayerInputControl playerInput;
+    private Animator anim;
+    public Transform playerTrans;
+    public GameObject signSprite;
+    private bool canPress;
+
+    private void Awake()
+    {
+        //anim = GetComponentInChildren<Animator>();
+        anim = signSprite.GetComponent<Animator>();
+
+        playerInput = new PlayerInputControl();
+        playerInput.Enable();
+    }
+
+    private void OnEnable()
+    {
+        InputSystem.onActionChange += OnActionChange;
+    }
+
+    private void Update()
+    {
+        signSprite.GetComponent<SpriteRenderer>().enabled = canPress;
+        signSprite.transform.localScale = playerTrans.localScale;
+    }
+    private void OnActionChange(object obj, InputActionChange actionChange)
+    {
+        if (actionChange == InputActionChange.ActionStarted)
+        {
+            //Debug.Log(((InputAction)obj).activeControl.device);
+
+            var d = ((InputAction)obj).activeControl.device;
+
+            switch (d.device)
+            {
+                case Keyboard:
+                    anim.Play("keyboard");
+                    break;
+                case DualShockGamepad:
+                    anim.Play("ps");
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            canPress = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canPress = false;
+    }
+}
