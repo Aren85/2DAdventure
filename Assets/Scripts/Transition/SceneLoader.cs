3b2a4a9683f973e6f22f8de01ba051fa6d9c139a
a00123561574@DESKTOP-D9GN4J5 <a00123561574@gmail.com>
2024-06-21 20:13:13 +0800

    人物数值及场景的保存加载

--- Assets/Scripts/Transition/SceneLoader.cs
+++ Assets/Scripts/Transition/SceneLoader.cs
@@ -7,7 +7,7 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class SceneLoader : MonoBehaviour, ISaveable
 {
     public Transform playerTrans;
     public Vector3 firstPosition;
@@ -50,11 +50,17 @@ public class SceneLoader : MonoBehaviour
     {
         loadEventSO.LoadRequestEvent += OnLoadRequestEvent;
         newGameEvent.OnEventRaised += NewGame;
+
+        ISaveable saveable = this;
+        saveable.RegisterSaveData();
     }
     private void OnDisable()
     {
         loadEventSO.LoadRequestEvent -= OnLoadRequestEvent;
         newGameEvent.OnEventRaised -= NewGame;
+
+        ISaveable saveable = this;
+        saveable.UnRegisterSaveData();
     }
 
     private void NewGame()
@@ -137,6 +143,28 @@ public class SceneLoader : MonoBehaviour
         {
             //場景加載完成後事件
             afterSceneLoadedEvent.RaiseEvent();
+        }
+    }
+
+    public DataDefinition GetDataID()
+    {
+        return GetComponent<DataDefinition>();
+    }
+
+    public void GetSaveData(Data data)
+    {
+        data.SaveGameScene(currentLoadScene);
+    }
+
+    public void LoadData(Data data)
+    {
+        var playerID = playerTrans.GetComponent<DataDefinition>().ID;
+        if (data.characterPosDict.ContainsKey(playerID))
+        {
+            positionToGo = data.characterPosDict[playerID];
+            sceneToLoad = data.GetSavedScene();
+
+            OnLoadRequestEvent(sceneToLoad, positionToGo, true);
         }
     }
 }
